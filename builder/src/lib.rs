3bf94aa6adb6c9fe0e148eb6b7ffef1dc20aece2
sh1nduu <sh1nduu@users.noreply.github.com>
2024-01-31 09:32:28 +0900

    Implement initial derive macro for Builder pattern
    - Set up the `quote` and `syn` crate dependencies for macro expansion.
    - Test that the `01-parse.rs` test case now passes with the stubbed `derive_builder` macro.
    - Added a placeholder macro expansion in `derive` to facilitate further development.
    - Enable the `01-parse.rs` test in `progress.rs` to track progress.
    
    This commit sets the groundwork for handling the `Builder` derive macro by setting up necessary dependencies and creating a stub for the macro's expansion logic.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,14 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let _ = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let expanded = quote! {
+
+    };
+
+    TokenStream::from(expanded)
 }
