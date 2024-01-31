fb666fb87756fb4a012cd87a733217292ddf61e8
sh1nduu <sh1nduu@users.noreply.github.com>
2024-02-01 01:33:05 +0900

    Add proc-macro2 as a dependency and implement initial builder pattern structures
    - Include the proc-macro2 crate to handle token streams more flexibly.
    - Define a skeleton for the derived Builder with optional fields for each struct field.
    - Implement the `builder` method in the main struct to instantiate the builder with all fields set to `None`.
    - Update the integration test to begin testing builder creation.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,14 +1,49 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro2::{Ident, Span};
+use quote::{quote, quote_spanned};
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Field, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
+    let builder_fields = get_struct_fields(&input.data).map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote_spanned! {f.span()=>
+            #name: Option<#ty>
+        }
+    });
+    let initial_fields = get_struct_fields(&input.data).map(|f| {
+        let name = &f.ident;
+        quote_spanned! {f.span()=>
+            #name: None
+        }
+    });
 
     let expanded = quote! {
-
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#initial_fields),*
+                }
+            }
+        }
+        struct #builder_name {
+            #(#builder_fields),*
+        }
     };
 
     TokenStream::from(expanded)
+}
+
+fn get_struct_fields(data: &Data) -> impl Iterator<Item = &Field> {
+    match data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => fields.named.iter(),
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
 }
