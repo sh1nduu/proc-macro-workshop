65c7a4156811627d9865943ac36ac5fea4610528
sh1nduu <sh1nduu@users.noreply.github.com>
2024-02-04 07:22:29 +0900

    Enhance builder macro to handle optional fields gracefully
    - Modify macro to keep Option-wrapped type fields intact
    - Implement setter methods for inner types of Option fields
    - Add helper functions to check and extract types wrapped in Option
    - Activate unit test for optional field support

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,10 @@
 use proc_macro::TokenStream;
 use proc_macro2::{Ident, Span};
 use quote::{quote, quote_spanned};
-use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Field, Fields};
+use syn::{
+    parse_macro_input, spanned::Spanned, Data, DeriveInput, Field, Fields, GenericArgument,
+    PathArguments, Type, TypePath,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -11,28 +14,47 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields = get_struct_fields(&input.data).map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        quote_spanned! {f.span()=>
-            #name: Option<#ty>
+        if is_option(ty) {
+            quote_spanned! {f.span() => #name: #ty }
+        } else {
+            quote_spanned! {f.span()=> #name: Option<#ty> }
         }
     });
     let builder_impls = get_struct_fields(&input.data).map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        quote_spanned! {f.span()=>
-            fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+        if is_option(ty) {
+            let inner = get_option_inner_type(ty).unwrap();
+            quote_spanned! {f.span()=>
+                fn #name(&mut self, #name: #inner) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote_spanned! {f.span()=>
+                fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }
     });
     let builder_build_unwrap_fields = get_struct_fields(&input.data).map(|f| {
         let name = &f.ident;
-        let error_msg = format!(
-            "Insufficient field: {}",
-            name.clone().map(|f| f.to_string()).unwrap_or_default()
-        );
-        quote_spanned! {f.span()=>
-            let Some(ref #name) = self.#name else { return Err(#error_msg.into()) };
+        let ty = &f.ty;
+        if is_option(ty) {
+            quote_spanned! {f.span() =>
+                let #name = self.#name.clone();
+            }
+        } else {
+            let error_msg = format!(
+                "Insufficient field: {}",
+                name.clone().map(|f| f.to_string()).unwrap_or_default()
+            );
+            quote_spanned! {f.span()=>
+                let Some(ref #name) = self.#name else { return Err(#error_msg.into()) };
+            }
         }
     });
     let builder_build_field_names = get_struct_fields(&input.data).map(|f| {
@@ -81,4 +103,30 @@ fn get_struct_fields(data: &Data) -> impl Iterator<Item = &Field> {
         },
         _ => unimplemented!(),
     }
+}
+
+fn is_option(ty: &Type) -> bool {
+    let Type::Path(TypePath { path, .. }) = ty else {
+        return false;
+    };
+    let Some(segment) = path.segments.first() else {
+        return false;
+    };
+    segment.ident.to_string() == "Option".to_string()
+}
+
+fn get_option_inner_type(ty: &Type) -> Option<Type> {
+    let Type::Path(TypePath { path, .. }) = ty else {
+        return None;
+    };
+    let Some(segment) = path.segments.first() else {
+        return None;
+    };
+    let PathArguments::AngleBracketed(ref arguments) = segment.arguments else {
+        return None;
+    };
+    let Some(GenericArgument::Type(generic_type)) = arguments.args.first() else {
+        return None;
+    };
+    Some(generic_type.clone())
 }
