cf95898223d2ceb0d76f3a5a1e33af2c7a7d97b1
sh1nduu <sh1nduu@users.noreply.github.com>
2024-02-09 05:17:06 +0900

    Add doc comments

--- builder/src/builder.rs
+++ builder/src/builder.rs
@@ -2,12 +2,16 @@ use proc_macro2::{Ident, Span, TokenStream};
 use quote::{format_ident, quote, quote_spanned};
 use syn::{spanned::Spanned, Data, DataStruct, DeriveInput, Error, Field, Fields, Result, Type};
 
-struct BuilderInput {
+/// `BuilderName` holds the identifiers for the target and the associated builder.
+struct BuilderName {
+    /// The identifier of the target type for which the builder is created.
     target_name: Ident,
+    /// The identifier of the builder structure associated with the target type.
     builder_name: Ident,
 }
 
-impl BuilderInput {
+impl BuilderName {
+    /// Parses the provided `DeriveInput` to construct a `BuilderName` instance.
     fn parse(input: &DeriveInput) -> Self {
         let target_name = input.ident.clone();
         let builder_name = format_ident!("{}Builder", target_name);
@@ -18,11 +22,13 @@ impl BuilderInput {
     }
 }
 
+/// `StructFields` holds the fields of the struct.
 struct StructFields {
     fields: Vec<StructField>,
 }
 
 impl StructFields {
+    /// Parses the provided `DeriveInput` to construct a `StructFields` instance.
     fn parse(input: &DeriveInput) -> Result<Self> {
         let Data::Struct(DataStruct {
             fields: Fields::Named(ref fields),
@@ -42,24 +48,34 @@ impl StructFields {
             .map(|fields| Self { fields })
     }
 
+    /// Generates a `TokenStream` of the builder field declarations.
     fn builder_fields(&self) -> TokenStream {
         let fields = self.fields.iter().map(StructField::builder_field);
         quote! { #(#fields)* }
     }
 
+    /// Generates a `TokenStream` of the builder fields initialized with default values, typically `None`.
     fn init_fields(&self) -> TokenStream {
         let fields = self.fields.iter().map(StructField::init_field);
         quote! { #(#fields)* }
     }
 }
 
+/// `StructField` holds metadata for a field of the provided struct.
 struct StructField {
+    /// The name of the field.
     name: Ident,
+    /// The type of the field.
     ty: Type,
+    /// The location of the field in the source code.
     span: Span,
 }
 
 impl StructField {
+    /// Parses the provided `syn::Field` to construct a `StructField` instance.
+    ///
+    /// This function takes a reference to a `syn::Field`, and construct a `StructField` containing metadata.
+    /// If the field does not have a name, it returns an error.
     fn parse(f: &Field) -> Result<Self> {
         let Some(name) = f.ident.clone() else {
             return Err(Error::new_spanned(f, "Missing field name"));
@@ -69,6 +85,7 @@ impl StructField {
         Ok(Self { name, ty, span })
     }
 
+    /// Generates a `TokenStream` of the declaration for the builder struct field.
     fn builder_field(&self) -> TokenStream {
         let name = &self.name;
         let ty = &self.ty;
@@ -77,6 +94,7 @@ impl StructField {
         }
     }
 
+    /// Generates a `TokenStream` of a struct field with a default value, typically `None`.
     fn init_field(&self) -> TokenStream {
         let name = &self.name;
         quote_spanned! {self.span=>
@@ -85,8 +103,9 @@ impl StructField {
     }
 }
 
+/// Generates a `TokenStream` representing the derived builder struct for a procedural macro.
 pub(crate) fn expand(input: DeriveInput) -> Result<TokenStream> {
-    let builder = BuilderInput::parse(&input);
+    let builder = BuilderName::parse(&input);
     let target_name = &builder.target_name;
     let builder_name = &builder.builder_name;
     let struct_fields = StructFields::parse(&input)?;
@@ -109,43 +128,33 @@ pub(crate) fn expand(input: DeriveInput) -> Result<TokenStream> {
 
 #[cfg(test)]
 mod expand {
-    use std::iter::zip;
-
     use super::*;
     use quote::ToTokens;
-    use syn::DeriveInput;
-
-    macro_rules! quote_derive_input {
-        ($($tt:tt)*) => {
-            syn::parse2::<DeriveInput>(quote! { $($tt)* })
-        };
-    }
+    use std::iter::zip;
+    use syn::{parse_quote, DeriveInput};
 
     #[test]
-    fn builder_input_parses_struct_names() -> Result<()> {
-        let input = quote_derive_input! {
-            #[derive(Builder)]
+    fn builder_input_parses_struct_names() {
+        let input: DeriveInput = parse_quote! {
             pub struct Command { }
-        }?;
+        };
 
-        let builder_input = BuilderInput::parse(&input);
+        let builder_input = BuilderName::parse(&input);
 
         assert_eq!(builder_input.target_name, "Command");
         assert_eq!(builder_input.builder_name, "CommandBuilder");
-
-        Ok(())
     }
 
     #[test]
     fn struct_fields_parses_each_fields() -> Result<()> {
-        let input = quote_derive_input! {
+        let input: DeriveInput = parse_quote! {
             struct Command {
                 executable: String,
                 args: Vec<String>,
                 env: Vec<String>,
                 current_dir: String,
             }
-        }?;
+        };
         let expectations = [
             ["executable", "String"],
             ["args", "Vec < String >"],
